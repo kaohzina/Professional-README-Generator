a2132262f606d05d025e36fad557b779522ed11d
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-18 20:32:12 -0500

    good

--- Develop/index.js
+++ Develop/index.js
@@ -124,5 +124,5 @@ function init() {}
 
 // Function call to initialize app
 init() {
-  return inquirer.prompt(questions).then(response => {readMeContent = response},
+  return inquirer.prompt(questions).then(response => {readMeContent = response}
   };
