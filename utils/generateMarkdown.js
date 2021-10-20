c9c5d20a4d6078127534cd161d383a462a24c66e
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-20 10:46:45 -0500

    more prompts to add to the readme that is generated at the end of questions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,6 +20,51 @@ const createInstall = (data) => {
 ${data.installation}`;
 };
 
+const createUsage = (data) => {
+  if (!data.tableOfContents.includes("Usage")) {
+    return "";
+  }
+  return `
+  ## Usage
+  ${data.usage}`
+}
+
+const createLicense =(data) => {
+  if (!data.tableOfContents.includes("License")) {
+    return "";
+  }
+  return `
+  ## License
+  ${data.license}`
+}
+
+const createContributing = (data) => {
+  if (!data.tableOfContents.includes("Contributing")){
+    return "";
+  }
+  return `
+  ## Contributing
+  ${data.contributing}`
+}
+
+const createTests = (data) => {
+  if (!data.tableOfContents.includes("Tests")) {
+    return "";
+  }
+  return `
+  ## Tests
+  ${data.tests}`
+}
+
+const createQuestions = (data) => {
+  if (!data.tableOfContents.includes("Questions")){
+    return "";
+  }
+  return`
+  ## Questions
+  ${data.questions}
+  `
+}
 function generateMarkdown(data) {
   return `# ${data.title}
 
